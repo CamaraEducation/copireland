5171315e1880a42c2e70c9b983751073636de927
unknown <yaredayele@camara.org>
2019-02-19 15:26:28 +0200

    delete .bak files

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 			<span><img src=	"<?php echo get_template_directory_uri();  ?>/images/techspacelogo.png" class="img-responsive" width="125" height="125" alt="COP"></span>
 		</div>
 		<div class="col-xs-6 col-centered" style="padding-left: 12px;">
-			<p class="profile-name">Kerri-ellen Casey</p>
+			<p class="profile-name">Yared -ellen Casey</p>
 			<p class="work-title">Project Officer @ Camara</p>
 		</div>
 	</section>
@@ -22,7 +22,6 @@ foreach ( $tax_terms as $term ) {
 <?php
 }
 ?>
-
 
 
 	</nav><!-- End PATH WYA NAGIVATION -->
